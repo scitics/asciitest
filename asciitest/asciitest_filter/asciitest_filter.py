5828cc9ac7137389d72dc8a6ba090835f15052ca
frans <frans.fuerst@scitics.de>
2014-01-31 15:12:48 +0100

    added test timeouts

--- asciitest/asciitest_filter/asciitest_filter.py
+++ asciitest/asciitest_filter/asciitest_filter.py
@@ -232,8 +232,11 @@ def code_filter():
         #          cmake stuff outside
 
         test_list_file.write(
-        'add_test(asciitest.%s_%s \"%s\" "${CMAKE_CURRENT_SOURCE_DIR}/env_run.py" "%s")\n' % (
+            'add_test(asciitest.%s_%s \"%s\" "${CMAKE_CURRENT_SOURCE_DIR}/env_run.py" "%s")\n' % (
                 document_name, test_name, sys.executable.replace("\\", "/"), test_filename))
+        test_list_file.write(
+            'set_tests_properties(asciitest.%s_%s PROPERTIES TIMEOUT 10)\n' % (
+                document_name, test_name))
 
         logging.info( "create a python script called '%s'", test_filename )
         logging.debug( "filename hash '%s'", save_cmake_filename(input_file) )
